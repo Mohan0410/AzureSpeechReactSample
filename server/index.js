eaebd8dff1f3c9c99248b926d02d064345368533
M Mohan Shankar <mohanshankarmummana@gmail.com>
2023-12-21 15:04:31 +0530

    removed env

--- server/index.js
+++ server/index.js
@@ -10,8 +10,8 @@ app.use(pino);
 
 app.get('/api/get-speech-token', async (req, res, next) => {
     res.setHeader('Content-Type', 'application/json');
-    const speechKey = process.env.SPEECH_KEY;
-    const speechRegion = process.env.SPEECH_REGION;
+    const speechKey = process.env.ed54381d9d224a0bb96d00980378ec83;
+    const speechRegion = process.env.eastus;
 
     if (speechKey === 'paste-your-speech-key-here' || speechRegion === 'paste-your-speech-region-here') {
         res.status(400).send('You forgot to add your speech key or region to the .env file.');
